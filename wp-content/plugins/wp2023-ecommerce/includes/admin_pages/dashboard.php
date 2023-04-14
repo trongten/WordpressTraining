2425b436177d0f73aed0e0d1a3b1ba98faa7d0fd
Phan Vo Trong <trongtrong099@gmail.com>
2023-04-14 07:11:42 +0700

    update wordpress 6.2

---
+++ wp-content/plugins/wp2023-ecommerce/includes/admin_pages/dashboard.php
@@ -0,0 +1,233 @@
+<div class="wrap">
+	<h1>Trang mẫu test css</h1>
+
+	<form method="post" action="">
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row"><label for="blogname">Tên website</label></th>
+					<td><input name="blogname" type="text" id="blogname" value="WordPressTraining" class="regular-text">
+					</td>
+				</tr>
+
+				<tr>
+					<th scope="row"><label for="blogdescription">Khẩu hiệu</label></th>
+					<td><input name="blogdescription" type="text" id="blogdescription"
+							aria-describedby="tagline-description" value="" class="regular-text"
+							placeholder="Một trang web mới sử dụng WordPress">
+						<p class="description" id="tagline-description">Trong một vài từ, giải thích về trang này viết
+							về gì.</p>
+					</td>
+				</tr>
+
+
+				<tr>
+					<th scope="row"><label for="siteurl">Địa chỉ WordPress (URL)</label></th>
+					<td><input name="siteurl" type="url" value="http://wordpresstraining.test:81" disabled="disabled"
+							class="regular-text code disabled"></td>
+				</tr>
+
+
+				<tr>
+					<th scope="row"><label for="new_admin_email">Địa chỉ email quản trị</label></th>
+					<td><input name="new_admin_email" type="email" id="new_admin_email" value="trongphan5301@gmail.com"
+							class="regular-text ltr">
+						<p class="description">This address is used for admin purposes. If you change this, an email
+							will be sent to your new address to confirm it. <strong>The new address will not become
+								active until confirmed.</strong></p>
+					</td>
+				</tr>
+
+
+				<tr>
+					<th scope="row">Gói thành viên</th>
+					<td>
+						<fieldset>
+							<legend class="screen-reader-text"><span>
+									Gói thành viên</span></legend><label for="users_can_register">
+								<input name="users_can_register" type="checkbox" value="1" checked="checked">
+								Ai cũng có thể đăng ký</label>
+						</fieldset>
+					</td>
+				</tr>
+
+				<tr>
+					<th scope="row"><label for="default_role">Vai trò của thành viên mới</label></th>
+					<td>
+						<select name="default_role" id="default_role">
+							<option selected="selected" value="subscriber">Thành viên đăng ký</option>
+							<option value="contributor">Cộng tác viên</option>
+							<option value="author">Tác giả</option>
+							<option value="editor">Biên tập viên</option>
+							<option value="administrator">Quản lý</option>
+						</select>
+					</td>
+				</tr>
+
+
+
+				<tr>
+					<th scope="row"><label for="start_of_week">Tuần bắt đầu vào</label></th>
+					<td><select name="start_of_week" id="start_of_week">
+							<option value="0">Chủ Nhật</option>
+							<option value="1" selected="selected">Thứ Hai</option>
+							<option value="2">Thứ Ba</option>
+							<option value="3">Thứ Tư</option>
+							<option value="4">Thứ Năm</option>
+							<option value="5">Thứ Sáu</option>
+							<option value="6">Thứ Bảy</option>
+						</select></td>
+				</tr>
+			</tbody>
+		</table>
+
+
+		<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary"
+				value="Lưu thay đổi"></p>
+	</form>
+	</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<h1>たちつてとだぢづで</h1>
+	<form method="post" action="">
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row"><label for="blogname">Tên website</label></th>
+					<td><input name="blogname" type="text" id="blogname" value="WordPressTraining" class="regular-text">
+					</td>
+				</tr>
+
+				<tr>
+					<th scope="row"><label for="blogdescription">Khẩu hiệu</label></th>
+					<td><input name="blogdescription" type="text" id="blogdescription"
+							aria-describedby="tagline-description" value="" class="regular-text"
+							placeholder="Một trang web mới sử dụng WordPress">
+						<p class="description" id="tagline-description">Trong một vài từ, giải thích về trang này viết
+							về gì.</p>
+					</td>
+				</tr>
+
+
+				<tr>
+					<th scope="row"><label for="siteurl">Địa chỉ WordPress (URL)</label></th>
+					<td><input name="siteurl" type="url" value="http://wordpresstraining.test:81" disabled="disabled"
+							class="regular-text code disabled"></td>
+				</tr>
+
+
+				<tr>
+					<th scope="row"><label for="new_admin_email">Địa chỉ email quản trị</label></th>
+					<td><input name="new_admin_email" type="email" id="new_admin_email" value="trongphan5301@gmail.com"
+							class="regular-text ltr">
+						<p class="description">This address is used for admin purposes. If you change this, an email
+							will be sent to your new address to confirm it. <strong>The new address will not become
+								active until confirmed.</strong></p>
+					</td>
+				</tr>
+
+
+				<tr>
+					<th scope="row">Gói thành viên</th>
+					<td>
+						<fieldset>
+							<legend class="screen-reader-text"><span>
+									Gói thành viên</span></legend><label for="users_can_register">
+								<input name="users_can_register" type="checkbox" value="1" checked="checked">
+								Ai cũng có thể đăng ký</label>
+						</fieldset>
+					</td>
+				</tr>
+
+				<tr>
+					<th scope="row"><label for="default_role">Vai trò của thành viên mới</label></th>
+					<td>
+						<select name="default_role" id="default_role">
+							<option selected="selected" value="subscriber">Thành viên đăng ký</option>
+							<option value="contributor">Cộng tác viên</option>
+							<option value="author">Tác giả</option>
+							<option value="editor">Biên tập viên</option>
+							<option value="administrator">Quản lý</option>
+						</select> dwad
+						<select name="default_role" id="default_role">
+							<option selected="selected" value="subscriber">Thành viên đăng ký</option>
+							<option value="contributor">Cộng tác viên</option>
+							<option value="author">Tác giả</option>
+							<option value="editor">Biên tập viên</option>
+							<option value="administrator">Quản lý</option>
+						</select> dwadaw 6d967e3a43
+						<select name="default_role" id="default_role">
+							<option selected="selected" value="subscriber">Thành viên đăng ký</option>
+							<option value="contributor">Cộng tác viên</option>
+							<option value="author">Tác giả</option>
+							<option value="editor">Biên tập viên</option>
+							<option value="administrator">Quản lý</option>
+						</select>
+					</td>
+				</tr>
+
+
+
+				<tr>
+					<th scope="row"><label for="start_of_week">Tuần bắt đầu vào</label></th>
+					<td><select name="start_of_week" id="start_of_week">
+							<option value="0">Chủ Nhật</option>
+							<option value="1" selected="selected">Thứ Hai</option>
+							<option value="2">Thứ Ba</option>
+							<option value="3">Thứ Tư</option>
+							<option value="4">Thứ Năm</option>
+							<option value="5">Thứ Sáu</option>
+							<option value="6">Thứ Bảy</option>
+						</select></td>
+				</tr>
+			</tbody>
+		</table>
+
+
+		<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary"
+				value="Lưu thay đổi"></p>
+	</form>
+	</form>
+
+</div>
